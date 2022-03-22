7b4dc065ed4e79adc47f797e0eecb85b6fd943a7
VictorTruong18 <victor.truong18@gmail.com>
2022-03-22 14:58:48 +0100

    Add variety in the junk food
    Add tacos, and donut.

--- js/food.js
+++ js/food.js
@@ -2,6 +2,7 @@
 // Positioning
 const foodOrginX = 470;
 const foodType = ['FRUIT', 'JUNK'];
+const junkFoodList = ['DONUT','TACOS'];
 const foodSpeed = 1;
 
 
@@ -13,7 +14,12 @@ class Food {
             this.food = new PIXI.Sprite.from("../images/ananas.png");
         }
         if (food == "JUNK") {
-            this.food = new PIXI.Sprite.from("../images/donut.png");
+            const junkFood = junkFoodList[Math.floor(Math.random() * junkFoodList.length)];
+            if(junkFood == "DONUT")
+                this.food = new PIXI.Sprite.from("../images/donut.png");
+            if(junkFood == "TACOS")
+                this.food = new PIXI.Sprite.from("../images/tacos.png");
+
         }
         this.food.x = foodOrginX;
         this.food.y = Math.floor(Math.random() * 640);
